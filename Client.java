5a9e24ff558a6f9582378ff9168553818eb06e5b
Nazwright1 <43227068+Nazwright1@users.noreply.github.com>
2021-02-10 10:41:57 -0500

    first commit

---
+++ Client.java
@@ -0,0 +1,13 @@
+package homework1;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author Naz Wright
+ */
---
+++ Server.java
@@ -0,0 +1,12 @@
+package homework1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Naz Wright
+ */
